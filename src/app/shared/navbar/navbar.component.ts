64666a2a356b02b6663765211730a050d76ee93a
Jonas Scholz <jonas.scholz2@vtg.com>
2020-09-13 21:34:47 +0200

    Add correct form for song selection

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -15,19 +15,16 @@ export class NavbarComponent implements OnInit {
     constructor(public location: Location, private router: Router) {
     }
 
+
+    navigateToPage() {
+        console.log("Navigate!")
+        this.isCollapsed = false;
+      }
+
     ngOnInit() {
       this.router.events.subscribe((event) => {
         this.isCollapsed = true;
-        if (event instanceof NavigationStart) {
-           if (event.url != this.lastPoppedUrl)
-               this.yScrollStack.push(window.scrollY);
-       } else if (event instanceof NavigationEnd) {
-           if (event.url == this.lastPoppedUrl) {
-               this.lastPoppedUrl = undefined;
-               window.scrollTo(0, this.yScrollStack.pop());
-           } else
-               window.scrollTo(0, 0);
-       }
+      
      });
      this.location.subscribe((ev:PopStateEvent) => {
          this.lastPoppedUrl = ev.url;
--- src/app/song-selection/song-selection.component.ts
+++ src/app/song-selection/song-selection.component.ts
@@ -9,6 +9,35 @@ export class SongSelectionComponent implements OnInit {
 
     constructor() { }
 
+    currentAttendee: Attendee =  {
+        id: "1234",
+        name: "Jonas",
+        isCurrentlyPerforming: false,
+        song: {
+          originalArtist: "Fleetwood Mac", 
+          name: "Everyhwere", 
+          youtubeKaraokeLink: "https://www.youtube.com/watch?v=I2rReu-Wzak"
+        }, 
+        receivedVotes: []
+      }
+
+    currentCompetition: Karaoke =  {
+        name: "Charity Karaoke II", 
+        description: "Coole Sache!", 
+        id: "1234", 
+        date: new Date("2020-09-13")
+      }
+
+    songFormModel = this.currentAttendee.song
+
+    saveSong() {
+        alert("Going to save " + JSON.stringify(this.songFormModel))
+    }
+
     ngOnInit() {}
 
+    submitted = false;
+
+    onSubmit() { this.submitted = true; }
+  
 }
